ac61e00012a0ccbb78e822271f0a0521b5c6fee1
Pratik Gautam <exploringpratik@gmail.com>
2019-07-15 09:35:13 +0545

    Initial

---
+++ main1.py
@@ -0,0 +1,89 @@
+import serial
+import requests
+import time
+
+millis = int(round(time.time() * 1000))
+x = int(millis)
+
+localMilli = 20700000 + x
+
+heartBeat = 0
+bodyTempr = 0
+geoLocation = []
+surrTempr = 0
+
+jsonUpdateAnimal = {
+    "heart_beat": heartBeat,
+    "body_tempr": bodyTempr,
+    "surr_tempr": surrTempr,
+    "geo_location": geoLocation,
+    "timestramp": localMilli
+}
+
+jsonPostHeartBeat = {
+    "timestramp": localMilli,
+    "heart_beat": heartBeat
+}
+
+jsonPostBodyTempr = {
+    "timestramp": localMilli,
+    "body_tempr": bodyTempr
+}
+
+jsonPostSurrTempr = {
+    "timestramp": localMilli,
+    "surr_tempr": surrTempr
+}
+
+jsonPostGeoLocation = {
+    "timestramp": localMilli,
+    "geo_location": geoLocation
+}
+
+
+def readSerialData():
+    ser = serial.Serial('COM8', 9600)
+    while 1:
+        print(ser.read(1))
+
+
+def updateAnimal(jsonFromFunction):
+    url = "https://animalmonitoringsystem.herokuapp.com/api/updateanimal/5d29fe2e79ed0d22e0d25ac3"
+
+    r = requests.put(url, json=jsonFromFunction)
+    print(r.status_code, r.reason)
+
+
+def postHeartBeat(jsonFromFunction):
+    url = "https://animalmonitoringsystem.herokuapp.com/api/postheartbeat/5d29fe2e79ed0d22e0d25ac3"
+
+    r = requests.post(url, json=jsonFromFunction)
+    print(r.status_code, r.reason)
+
+
+def postbodytempr(jsonFromFunction):
+    url = "https://animalmonitoringsystem.herokuapp.com/api/postbodytempr/5d29fe2e79ed0d22e0d25ac3"
+
+    r = requests.post(url, json=jsonFromFunction)
+    print(r.status_code, r.reason)
+
+
+def postsurrtempr(jsonFromFunction):
+    url = "https://animalmonitoringsystem.herokuapp.com/api/postsurrtempr/5d29fe2e79ed0d22e0d25ac3"
+
+    r = requests.post(url, json=jsonFromFunction)
+    print(r.status_code, r.reason)
+
+
+def postgeolocation(jsonFromFunction):
+    url = "https://animalmonitoringsystem.herokuapp.com/api/postgeolocation/5d29fe2e79ed0d22e0d25ac3"
+
+    r = requests.post(url, json=jsonFromFunction)
+    print(r.status_code, r.reason)
+
+
+updateAnimal(jsonUpdateAnimal)
+postbodytempr(jsonPostBodyTempr)
+postgeolocation(jsonPostGeoLocation)
+postHeartBeat(jsonPostHeartBeat)
+postsurrtempr(jsonPostSurrTempr)
---
+++ main2.py
@@ -0,0 +1,4 @@
+import serial
+
+serialPort = serial.Serial(port="COM4", baudrate=115200,
+                           bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
